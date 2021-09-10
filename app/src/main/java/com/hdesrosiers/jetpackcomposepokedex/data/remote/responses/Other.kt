f19f202a498382da0b43025e5d1a9fde8d31fef0
Herve Desrosiers <hdesrosiers@stingray.com>
2021-09-10 07:55:17 -0400

    @SerializedName json data parameter values and palette dependency update

--- app/src/main/java/com/hdesrosiers/jetpackcomposepokedex/data/remote/responses/Other.kt
+++ app/src/main/java/com/hdesrosiers/jetpackcomposepokedex/data/remote/responses/Other.kt
@@ -1,6 +1,9 @@
 package com.hdesrosiers.jetpackcomposepokedex.data.remote.responses
 
+import com.google.gson.annotations.SerializedName
+
 data class Other(
     val dream_world: DreamWorld,
-    val official-artwork: OfficialArtwork
+    @SerializedName("official-artwork")
+    val officialArtwork: OfficialArtwork
 )
